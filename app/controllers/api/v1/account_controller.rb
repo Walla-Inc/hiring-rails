4bf1ef6f955d99b39dfc68f831a9eb12bb9b4d75
Marcus Salinas <markysalinas12@gmail.com>
2021-01-19 16:34:08 -0600

    Removed passing an ID for the #latest_tweets to using the current sign in user given that the bonus assignment was to increase the number of times fetched

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -9,24 +9,28 @@ class Api::V1::AccountController < ApplicationController
 
   # gets the 10 latest tweets from the account
   def latest_tweets
-    id = params.require(:id)
     client = Twitter::REST::Client.new do |config|
       config.consumer_key    = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
     end
 
     count = params[:count] || 10
-    time_line = client.user_timeline(id, {count: count})
+    time_line = client.user_timeline(current_user.twitter_handle, {count: count})
+    current_user.update(time_fetched: current_user.time_fetched+1)
     if time_line.class == Twitter::Error::NotFound
       render json: {status: 404, success: false, message: 'Twitter handle or id not valid'}
     else
       render json: {status: 200, success: true, message: 'Success', data: time_line.map{|a| a.text}.as_json}
     end
 
+
+
   rescue => e
     Rails.logger.error "account_controller#latest_tweets - Error: #{e.message}"
     render json: {status: 404, success: false, message: 'Unexpected error'}
   end
+
+
 
   private
 
