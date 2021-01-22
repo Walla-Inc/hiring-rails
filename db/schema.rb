97ff7d35158841075391907823757a989c7e6be9
Sam W <sam@sam-we.com>
2021-01-22 00:53:58 -0800

    Transients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_220612) do
+ActiveRecord::Schema.define(version: 2021_01_22_062014) do
+
+  create_table "twitter_handles", force: :cascade do |t|
+    t.string "twitter_handle"
+    t.integer "fetch_count", default: 0
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_twitter_handles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -28,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_01_18_220612) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "twitter_handles", "users"
 end
