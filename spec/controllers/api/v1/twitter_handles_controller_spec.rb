d685a94d8bda51164be86a3e6dd11335537bd8f2
Zolon <zolonsk8@gmail.com>
2021-01-31 11:27:22 -0600

    use factory bot

--- spec/controllers/api/v1/twitter_handles_controller_spec.rb
+++ spec/controllers/api/v1/twitter_handles_controller_spec.rb
@@ -1,13 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::TwitterHandlesController, type: :controller do
-  let(:email) { "test@hellowalla.com" }
-  let(:password) { "password" }
-  let(:password_confirmation) { "password" }
-  let(:first_name) { "Test" }
-  let(:last_name) { "User" }
-  let(:twitter_handle) { "planetzolon" }
-  let(:user) { User.create(email: email, password: password, password_confirmation: password_confirmation, first_name: first_name, last_name: last_name, twitter_handle: twitter_handle) }
+  let(:user) { create(:user) }
 
   before(:each) do
     sign_in user
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :user do
+    email { "test@hellowalla.com" }
+    password { "password" }
+    password_confirmation { "password" }
+    first_name { "Test" }
+    last_name { "User" }
+    twitter_handle { "planetzolon" }
   end
 end
